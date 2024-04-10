64743ee24fce8d3d3b1a5e3040636924b40a6dd5
Angel Ernesto Rivas Hernández <sanguispruinae@gmail.com>
2024-04-10 19:10:36 -0400

    Arreglo

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,6 @@ class _MyAppState extends State<MyApp> {
           children: [
             HomeScreen(),
             AlberguesScreen(),
-            LoginScreen(),
           ],
         ),
         bottomNavigationBar: Container(
